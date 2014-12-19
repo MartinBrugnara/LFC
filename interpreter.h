763b28dbdb97613017efb5691cab2ee85f0fe7f7
Martin Brugnara <martin.brugnara@gmail.com>
2014-12-19 15:49:00 +0100

    --

--- interpreter.h
+++ interpreter.h
@@ -1,9 +1,8 @@
 
+// Symbol Table (list)
 // Type for symrec
 typedef enum{INTTYPE, REALTYPE, BOOLTYPE} varTypeEnum; // old: basicType
-
-typedef struct symrec
-{
+typedef struct symrec {
 	char * name; // Comes from lexer
     union { // Comes from yacc & run time
         int     iVal;
@@ -14,6 +13,39 @@ typedef struct symrec
 
 	struct symrec *next;
 } symrec;
+
+/* Code node tree */
+
+/* used in the struct nodeType to define the type of node*/
+typedef enum {nodeCon, nodeId, nodeOpr} nodeEnum;
+typedef struct {
+    nodeEnum type;              /* type of node */
+
+    union {
+        conNodeType con;        /* constants */
+        idNodeType id;          /* identifiers */
+        oprNodeType opr;        /* operators */
+    };
+} nodeType;
+
+/* constants */
+typedef struct {
+    union {
+        int i;
+        float f;
+        int b;
+    }
+} conNodeType;
+
+/* identifiers */
+typedef struct {char * name;} idNodeType;
+
+/* operators */
+typedef struct {
+    char oper;                   /* operator */
+    int  nops;                   /* number of operands */
+    struct nodeType **op;	/* operands */
+} oprNodeType;
 
 
 void yyerror(const char *);
