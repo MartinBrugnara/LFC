c982ee7749357ade36c95b081fd19c2e8d70265d
Martin Brugnara <martin.brugnara@gmail.com>
2014-12-19 17:29:33 +0100

    wrtinng yeacc and rel func

--- interpreter.c
+++ interpreter.c
@@ -1,6 +1,8 @@
 #include "interpreter.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
 
 /* http://www.gnu.org/software/bison/manual/html_node/Error-Reporting.html
  * """ After yyerror returns to yyparse, the latter will attempt error recovery
@@ -20,14 +22,83 @@ void * xmalloc(size_t size) {
     return p;
 }
 
-nodeType *con(int value){
-    nodeType *p;
-    /* allocate node space in memory */
-    if((p=malloc(sizeof(nodeType))) == NULL){
-        yyerror("out of memory");
-    }
+/* Create a node containing a constant value */
+nodeType *con(void *value, varTypeEnum type){
+    nodeType *p = (nodeType*)xmalloc(sizeof(nodeType));
+
     /* copy information */
-    p->type = typeCon;
-    p->con.value = value;
+    p->type = nodeCon;
+    switch (type) {
+        case INTTYPE:
+            p->con.i = *(int*)value;
+            break;
+        case REALTYPE:
+            p->con.f = *(float*)value;
+            break;
+        case BOOLTYPE:
+            p->con.b = *(int*)value;
+            break;
+        default:
+            yyerror("Error handling constant type");
+    }
+    return p;
+}
+
+/* Get or Create a Variable in Symbol Table. Key = id. */
+nodeType * id(symrec * ide){
+    nodeType * p = (nodeType*)xmalloc(sizeof(nodeType));
+    p->type = nodeId;
+    p->id.name = (char*)xmalloc(sizeof(strlen(ide->name)) + 1);
+    strcpy(p->id.name, ide->name);
+
+    if (getsym(ide->name) == NULL) { // Symbol not yet in SymT
+        putsym(p->id.name);
+    }
+    return p;
+}
+
+// Search in Symbol Table (that is a fucking list) O(n)
+symrec * getsym(const char * const identifier) {
+    symrec *ptr;
+    for (ptr = symTable; ptr != (symrec *) 0; ptr = (symrec *)ptr->next){
+        if (strcmp (ptr->name, identifier) == 0)
+            return ptr; // found
+    }
+    return NULL; // not found
+}
+
+symrec * putsym(char const * identifier) {
+    symrec *ptr = (symrec *)xmalloc(sizeof (symrec));
+    ptr->name = (char *) malloc (strlen (identifier) + 1);
+    strcpy (ptr->name,identifier);
+    ptr->next = (struct symrec *)symTable;
+    symTable = ptr;
+    return ptr;
+}
+
+/* Create a operand node
+ * oper: operand
+ * nops: #operands
+ */
+nodeType *opr(int oper, int nops, ...){
+    /* (ap = argument pointer) va_list is used to declare a variable
+            which, from time to time, is referring to an argument*/
+    va_list ap;
+
+    nodeType *p;
+
+    p = (nodeType*)xmalloc(sizeof(nodeType));
+    p->opr.op = (struct nodeType**)xmalloc(nops*sizeof(nodeType));
+
+    p->type     = nodeOpr;
+    p->opr.oper = oper;
+    p->opr.nops = nops;
+
+    // *args (list) -> p->opr.op (array)
+    va_start(ap, nops);
+    for(int i=0; i<nops; i++)
+        p->opr.op[i]=va_arg(ap,nodeType*);
+    va_end(ap);
+
     return p;
 }
